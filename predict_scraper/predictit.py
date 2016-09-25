c73cbfa19727542c16d3d6f368d98cb513614b2b
Aaron Girard <aarongirard95@gmail.com>
2016-09-24 21:45:59 -0400

    new added time to logging

--- predict_scraper/predictit.py
+++ predict_scraper/predictit.py
@@ -1,11 +1,11 @@
-import requests, time, multiprocessing, os, sys
+import requests, time, multiprocessing, os, sys, threading
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from database import add_record_StateElectoralCollege_table
 
 def log(error):
   with open('log_exceptions.txt', 'a+') as f:
-    f.write(error+'\n')
+    f.write(str(str(int(time.time())) + ' :' + error+'\n'))
 
 #returns json of requested states' data
 def get_request_by_state(state):
@@ -30,7 +30,6 @@ def parse_json(json):
   return contracts
 
 def run_one_state(state):
-  print state
   #print ' requesting ', state
   req_json = get_request_by_state(state)
   if req_json != 0:
@@ -50,14 +49,14 @@ def main():
         t = threading.Thread(target=run_one_state, args=(line,))
         threads.append(t)
         try:
-          print 'starting thread'
+          #print 'starting thread'
           t.start()
         except e:
           log(str(e))
         #p = multiprocessing.Process(target=run_one_state, args=(line,))
         #p.start()
         #run_one_state(line)
-    time.sleep(3600)#60*60
+    time.sleep(60)#60*60
 
   
 if __name__ == '__main__':
